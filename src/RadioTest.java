16f43a83197b2119fda51261fe03f5afd001e1ed
sharlisbg <sharislizza15@gmail.com>
2023-01-15 16:40:54 -0600

    Ultimos Test

--- src/RadioTest.java
+++ src/RadioTest.java
@@ -38,14 +38,35 @@ public class RadioTest {
 	public void ForwardRadioAM() throws Exception {
 		radio.setFrequence("AM");
 		radio.Forward();
-		assertEquals(540, radio.getFrequence());
+		assertEquals(540, radio.getAMActualStation());
+	}
+
+	@Test
+	public void setUnaRadioAM() throws Exception {
+		radio.setFrequence("AM");
+		radio.setAMActualStation(1610);
+		assertEquals(1610, radio.getAMActualStation());
 	}
 
 	@Test
 	public void BackwardRadioAM() throws Exception {
 		radio.setFrequence("AM");
-		radio.setAMActualStation(530);
+		radio.setAMActualStation(1610);
+		radio.Backward();
+		assertEquals(1600, radio.getAMActualStation());
+	}
+
+	@Test
+	public void ForwardRadioFM() throws Exception {
+		radio.setFrequence("FM");
 		radio.Forward();
-		assertEquals(1610, radio.getFrequence());
+		assertEquals(88.1, radio.getFMActualStation(), 0.01);
+	}
+
+	@Test
+	public void setUnaRadioFM() throws Exception {
+		radio.setFrequence("FM");
+		radio.setFMActualStation(89.0);
+		assertEquals(89.0, radio.getAMActualStation(), 0.01);
 	}
 }
